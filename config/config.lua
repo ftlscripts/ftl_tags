4a224281288eb1acd50670ca9af9faf851280aa3
devman <117568096+ftlscripts@users.noreply.github.com>
2022-11-06 11:27:39 +0100

    Add files via upload

---
+++ config/config.lua
@@ -0,0 +1,27 @@
+Config = {}
+Config.SeeOwnLabel = true
+Config.SeeDistance = 100
+Config.TextSize = 1.0
+Config.ZOffset = 1.4
+Config.NearCheckWait = 500
+Config.TagByPermission = false --Using xPlayer.getPermissions() which is deprecated method for old ESX
+Config.GroupLabels = {
+    helper = '<font face="Fire Sans">HELPER',
+    mod = '<font face="Fire Sans">MODERATOR',
+    trialadmin = '<font face="Fire Sans">TRIAL ADMIN',
+    admin = '<font face="Fire Sans">ADMIN',
+    supervisoradmin = '<font face="Fire Sans">SUPERVISOR ADMIN',
+    zastupcehead = '<font face="Fire Sans">ZÁSTUPCE HEAD ADMINA',
+    headadmin = '<font face="Fire Sans">HEAD ADMIN',
+    dev = '<font face="Fire Sans">DEVELOPER',
+    management = '<font face="Fire Sans">MANAGEMENT',
+    owner = '<font face="Fire Sans">VEDENÍ',
+}
+
+Config.PermissionLabels = {
+    [1] = "HELPER",
+    [2] = "~g~MODERATOR",
+    [3] = "~b~ADMINISTRATOR",
+    [4] = "~r~GOD",
+    [5] = "~r~GOD",
+}
---
+++ server/main.lua
@@ -0,0 +1,32 @@
+ESX = nil
+AdminPlayers = {}
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterCommand('tag', function(source,args)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if AdminPlayers[source] == nil then
+        if Config.TagByPermission then
+            AdminPlayers[source] = {source = source, permission = xPlayer.getPermissions()}
+        else
+            AdminPlayers[source] = {source = source, group = xPlayer.getGroup()}
+        end
+
+        TriggerClientEvent('chat:addMessage',source, { args = { 'Tag', 'Právě jste si zapl tag' }, color = { 255, 50, 50 } })
+    else
+        AdminPlayers[source] = nil
+        TriggerClientEvent('chat:addMessage',source, { args = { 'Tag', 'Právě jste si vypnul tag' }, color = { 255, 50, 50 } })
+    end
+    TriggerClientEvent('ftl_tags:set_admins',-1,AdminPlayers)
+end)
+
+ESX.RegisterServerCallback('ftl_tags:getAdminsPlayers',function(source,cb)
+    cb(AdminPlayers)
+end)
+
+AddEventHandler('esx:playerDropped', function(source)
+    if AdminPlayers[source] ~= nil then
+        AdminPlayers[source] = nil
+    end
+    TriggerClientEvent('ftl_tags:set_admins',-1,AdminPlayers)
+end)
